b1ea3668445b241c4cbb7283f48491ae058ec814
Ansonator <ansonator@hotmail.com>
2023-10-10 14:58:21 -0500

    disabled server tests since they require DB

--- descent-api-server/src/test/java/org/datrunk/descent/server/repo/DaoTest.java
+++ descent-api-server/src/test/java/org/datrunk/descent/server/repo/DaoTest.java
@@ -3,20 +3,29 @@ package org.datrunk.descent.server.repo;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.stream.Stream;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.sql.DataSource;
 import javax.transaction.Transactional;
+import liquibase.exception.LiquibaseException;
 import org.datrunk.descent.entities.HeroCard;
 import org.datrunk.descent.entities.HeroItemClass;
 import org.datrunk.descent.entities.Item;
+import org.datrunk.naked.db.mysql.MySqlTestContainer;
 import org.datrunk.naked.server.repo.BaseRepositoryImpl;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
@@ -27,14 +36,25 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
 import org.springframework.hateoas.config.EnableHypermediaSupport.HypermediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.filter.ForwardedHeaderFilter;
 
-@SpringBootTest(classes = DaoTest.Config.class, webEnvironment = WebEnvironment.NONE)
+@SpringBootTest(webEnvironment = WebEnvironment.NONE)
 @ExtendWith({SpringExtension.class})
 @EnableTransactionManagement
 @Transactional
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ContextConfiguration(
+    initializers = {MySqlTestContainer.Factory.class},
+    classes = {DaoTest.Config.class})
+@EnableConfigurationProperties({DataSourceProperties.class})
+@ActiveProfiles("test")
+@Disabled(
+    "These test work only if the DB is running.  Execute mvn spring-boot:run -pl descent-api-server before these tests")
 public class DaoTest {
   @Configuration
   @EntityScan(basePackageClasses = {HeroCard.class})
@@ -46,6 +66,16 @@ public class DaoTest {
   @ComponentScan
   @EnableAutoConfiguration
   static class Config {
+    boolean initialized = false;
+
+    @Bean
+    public DataSource dataSource(MySqlTestContainer db) throws LiquibaseException, SQLException {
+      if (!initialized) {
+        initialized = true;
+      }
+      return db.getDataSource();
+    }
+
     @Bean
     RepositoryRestConfigurer repositoryRestConfigurer(EntityManager entityManager) {
       return RepositoryRestConfigurer.withConfig(
@@ -75,8 +105,9 @@ public class DaoTest {
   private @PersistenceContext EntityManager em;
 
   @BeforeEach
-  void before() {
+  void before(DataSource ds) {
     assertThat(em).isNotNull();
+    assertThat(ds).isNotNull();
   }
 
   @Test
